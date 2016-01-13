90989bd9e4954cb91ed0058379a855f13481430a
Scott Alan Miller <scott@ntg.co>
2016-01-13 14:23:15 -0500

    first commit

---
+++ xenorchestera_installer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cd /opt
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+sudo apt-get install --yes nodejs
+curl -o /usr/local/bin/n https://raw.githubusercontent.com/visionmedia/n/master/bin/n
+chmod +x /usr/local/bin/n
+n stable
+node -v
+npm -v
+sudo apt-get install --yes build-essential redis-server libpng12-dev git python-minimal
+git clone -b stable https://github.com/vatesfr/xo-server
+git clone -b stable https://github.com/vatesfr/xo-web
+cd xo-server
+sudo npm install && npm run build
+cp xo-server/sample.config.yaml xo-server/.xo-server.yaml
+sed /mounts/a\\"    '/': '/opt/xo-web/dist'" xo-server/.xo-server.yaml
+cd /opt/xo-web
+sudo npm i lodash.trim@3.0.1
+sudo npm install
+sudo npm run build
+cd /opt/xo-server
+sudo npm start
